5498885d729bcf136a2f8b87ecd32f61371f03f6
iamkai <u0251074@nkfust.edu.tw>
2017-10-01 13:53:46 +0800

    Make the calculate more extendable
    Because the calcualte.c is hard to extendable.
    When I create a new test case, the code will change a lot.
    After refactoring, I just maintain array when I create a new case.

--- calculate.c
+++ calculate.c
@@ -1,51 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+# define TEST_CASE_NUM 2
+typedef struct {
+    char output[20];
+    double sum_a;
+    double sum_f;
+} testcase;
 int main(void)
 {
-    FILE *fp = fopen("orig.txt", "r");
-    FILE *output = fopen("output.txt", "w");
-    if (!fp) {
-        printf("ERROR opening input file orig.txt\n");
-        exit(0);
-    }
-    int i = 0;
+    int i = 0,j = 0;
     char append[50], find[50];
-    double orig_sum_a = 0.0, orig_sum_f = 0.0, orig_a, orig_f;
-    for (i = 0; i < 100; i++) {
-        if (feof(fp)) {
-            printf("ERROR: You need 100 datum instead of %d\n", i);
-            printf("run 'make run' longer to get enough information\n\n");
-            exit(0);
-        }
-        fscanf(fp, "%s %s %lf %lf\n", append, find, &orig_a, &orig_f);
-        orig_sum_a += orig_a;
-        orig_sum_f += orig_f;
-    }
-    fclose(fp);
+    FILE *fp, *output;
 
-    fp = fopen("opt.txt", "r");
-    if (!fp) {
-        fp = fopen("orig.txt", "r");
+    testcase cases[TEST_CASE_NUM] = {
+        {"orig.txt"},
+        {"opt.txt"},
+    };
+
+
+    for (i = 0; i < TEST_CASE_NUM; i++) {
+        fp = fopen(cases[i].output, "r");
         if (!fp) {
-            printf("ERROR opening input file opt.txt\n");
+            printf("ERROR opening input file orig.txt\n");
             exit(0);
         }
-    }
-    double opt_sum_a = 0.0, opt_sum_f = 0.0, opt_a, opt_f;
-    for (i = 0; i < 100; i++) {
-        if (feof(fp)) {
-            printf("ERROR: You need 100 datum instead of %d\n", i);
-            printf("run 'make run' longer to get enough information\n\n");
-            exit(0);
+        double append_time = 0.0, find_time = 0.0;
+        for (j = 0; j < 100; j++) {
+            if (feof(fp)) {
+                printf("ERROR: You need 100 datum instead of %d\n", j);
+                printf("run 'make run' longer to get enough information\n\n");
+                exit(0);
+            }
+            fscanf(fp, "%s %s %lf %lf\n", append, find, &append_time, &find_time);
+            cases[i].sum_a += append_time;
+            cases[i].sum_f += find_time;
         }
-        fscanf(fp, "%s %s %lf %lf\n", append, find, &opt_a, &opt_f);
-        opt_sum_a += opt_a;
-        opt_sum_f += opt_f;
+        fclose(fp);
     }
-    fprintf(output, "append() %lf %lf\n",orig_sum_a / 100.0, opt_sum_a / 100.0);
-    fprintf(output, "findName() %lf %lf", orig_sum_f / 100.0, opt_sum_f / 100.0);
+
+    /* output */
+    output = fopen("output.txt", "w");
+
+    fprintf(output, "append()");
+    for (i = 0; i < TEST_CASE_NUM; i++) {
+        fprintf(output, " %lf", cases[i].sum_a / 100.0);
+    }
+
+    fprintf(output, "\nfindName()");
+    for (i = 0; i < TEST_CASE_NUM; i++) {
+        fprintf(output, " %lf", cases[i].sum_f / 100.0);
+    }
+
     fclose(output);
-    fclose(fp);
+
     return 0;
 }
